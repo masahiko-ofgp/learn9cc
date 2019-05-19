8967e8469c778540045d271314bde7cf3f5c2391
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2019-05-19 16:32:51 +0900

    四則演算のできる言語の作成

--- learn9cc.c
+++ learn9cc.c
@@ -40,7 +40,7 @@ typedef struct Node {
 
 void error(char *fmt, ...);
 void error_at(char *loc, char *msg);
-void tokenize();
+void tokenize(char *p);
 Node *new_node(int ty, Node *lhs, Node *rhs);
 Node *new_node_num(int val);
 int consume(int ty);
@@ -63,33 +63,10 @@ int main(int argc, char **argv) {
     printf(".global main\n");
     printf("main:\n");
 
-    if (tokens[0].ty != TK_NUM)
-        error_at(tokens[0].input, "Not number");
-    printf("  mov rax, %d\n", tokens[0].val);
 
-    int i = 1;
-    while (tokens[i].ty != TK_EOF) {
-        if (tokens[i].ty == '+') {
-            i++;
-            if (tokens[i].ty != TK_NUM)
-                error_at(tokens[i].input, "Not number");
-            printf("  add rax, %d\n", tokens[i].val);
-            i++;
-            continue;
-        }
+    gen(node);
 
-        if (tokens[i].ty == '-') {
-            i++;
-            if (tokens[i].ty != TK_NUM)
-                error_at(tokens[i].input, "Not number");
-            printf("  sub rax, %d\n", tokens[i].val);
-            i++;
-            continue;
-        }
-
-        error_at(tokens[i].input, "Unexpected token");
-    }
-
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
@@ -113,12 +90,19 @@ void error_at(char *loc, char *msg) {
     exit(1);
 }
 
-void tokenize() {
-    char *p = user_input;
+void tokenize(char *p) {
 
     int i = 0;
     while (*p) {
         if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '(' || *p == ')') {
+            tokens[i].ty = *p;
+            tokens[i].input = p;
+            i++;
             p++;
             continue;
         }
@@ -142,8 +126,8 @@ void tokenize() {
         error_at(p, "Couldn't tokenize");
     }
 
-        tokens[i].ty = TK_EOF;
-        tokens[i].input = p;
+    tokens[i].ty = TK_EOF;
+    tokens[i].input = p;
 }
 
 Node *new_node(int ty, Node *lhs, Node *rhs) {
