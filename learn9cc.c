5315b59b84d91bafcc9e381a81566ad44c427e13
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2019-05-28 13:55:08 +0900

    任意のサイズの入力の受け取り

--- learn9cc.c
+++ learn9cc.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-// トークンの型を表す値
+/* Token Type */
 enum {
-    TK_NUM = 256, // 整数トークン
-    TK_EOF,        // 入力の終わりを表すトークン
+    TK_NUM = 256,
+    TK_EOF,
     TK_EQ,
     TK_NE,
     TK_LT,
@@ -16,28 +16,19 @@ enum {
     TK_GE,
 };
 
-// トークンの型
-typedef struct {
-    int ty;  // トークンの型
-    int val;  // tyがTK_NUMの場合、その数値
-    char *input; // トークン文字列（エラーメッセージ用）
+typedef struct Token {
+    int ty;
+    int val;  /* "ty=TK_NUM" only */
+    char *input; /* If error, this value is used. */
 } Token;
 
-// 入力プログラム
-char *user_input;
-
-Token tokens[100];
-
-int pos = 0;
-
-// 可変長ベクタ
 typedef struct Vector {
     void **data;
     int len;
     int capacity;
 } Vector;
 
-// ASTのノードの型
+/* NODE TYPE */
 enum {
     ND_NUM = 256,
     ND_EQ,
@@ -49,15 +40,16 @@ enum {
 };
 
 typedef struct Node {
-    int ty;  // 演算子かND_NUM
+    int ty;
     struct Node *lhs;
     struct Node *rhs;
-    int val;  // tyがND_NUMの場合のみ使用
+    int val;  /* "ty=ND_NUM" only */
 } Node;
 
+
+/****** PROTO TYPE ******/
 void error(char *fmt, ...);
 void error_at(char *loc, char *msg);
-void tokenize(char *p);
 Node *new_node(int ty, Node *lhs, Node *rhs);
 Node *new_node_num(int val);
 int consume(int ty);
@@ -73,8 +65,17 @@ Vector *new_vector();
 void vec_push(Vector *vec, void *elem);
 int expect(int line, int expected, int actual);
 void runtest();
+Vector *tokenize(char *p);
+Token *add_token(Vector *v, int ty, char *input);
 
 
+/****** GLOBAL VARIABLES ******/
+int pos;
+char *user_input;
+Vector *tokens;
+
+
+/****** MAIN ******/
 int main(int argc, char **argv) {
     if (argc != 2) {
         error("ERROR: The number of arguments is incorrect.");
@@ -86,43 +87,45 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    tokenize(argv[1]);
+    pos = 0;
+    user_input = argv[1];
+    tokens = (Vector *)tokenize(user_input);
     Node *node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-
     gen(node);
 
     printf("  pop rax\n");
     printf("  ret\n");
+
     return 0;
 }
 
-// エラー報告するための関数
-// ap => argument pointer
+/* Reporting Error */
 void error(char *fmt, ...) {
-    va_list ap;
+    va_list ap;  /* argument pointer */
     va_start(ap, fmt);
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
 }
 
-// エラー箇所を報告するための関数
+/* Reporting Error place */
 void error_at(char *loc, char *msg) {
-    int pos = loc - user_input;
+    pos = loc - user_input;
     fprintf(stderr, "%s\n", user_input);
-    fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+    fprintf(stderr, "%*s", pos, "");
     fprintf(stderr, "^ %s\n", msg);
     exit(1);
 }
 
-void tokenize(char *p) {
+Vector *tokenize(char *p) {
 
-    int i = 0;
+    Vector *v = new_vector();
+    
     while (*p) {
         if (isspace(*p)) {
             p++;
@@ -130,87 +133,70 @@ void tokenize(char *p) {
         }
 
         if (strncmp(p, "==", 2) == 0) {
-            tokens[i].ty = TK_EQ;
-            tokens[i].input = p;
-            i++;
+            add_token(v, TK_EQ, p);
             p++;
             p++;
             continue;
         }
 
         if (strncmp(p, "!=", 2) == 0) {
-            tokens[i].ty = TK_NE;
-            tokens[i].input = p;
-            i++;
+            add_token(v, TK_NE, p);
             p++;
             p++;
             continue;
         }
 
         if (strncmp(p, "<=", 2) == 0) {
-            tokens[i].ty = TK_LE;
-            tokens[i].input = p;
-            i++;
+            add_token(v, TK_LE, p);
             p++;
             p++;
             continue;
         }
 
         if (strncmp(p, ">=", 2) == 0) {
-            tokens[i].ty = TK_GE;
-            tokens[i].input = p;
-            i++;
+            add_token(v, TK_GE, p);
             p++;
             p++;
             continue;
         }
 
         if (*p == '<') {
-            tokens[i].ty = TK_LT;
-            tokens[i].input = p;
-            i++;
+            add_token(v, TK_LE, p);
             p++;
             continue;
         }
 
         if (*p == '>') {
-            tokens[i].ty = TK_GT;
-            tokens[i].input = p;
-            i++;
+            add_token(v, TK_GT, p);
             p++;
             continue;
         }
 
         if (*p == '(' || *p == ')') {
-            tokens[i].ty = *p;
-            tokens[i].input = p;
-            i++;
+            add_token(v, *p, p);
             p++;
             continue;
         }
 
         if (*p == '+' || *p == '-' || *p == '*' || *p == '/') {
-            tokens[i].ty = *p;
-            tokens[i].input = p;
-            i++;
+            add_token(v, *p, p);
             p++;
             continue;
         }
 
         if (isdigit(*p)) {
-            tokens[i].ty = TK_NUM;
-            tokens[i].input = p;
-            tokens[i].val = strtol(p, &p, 10);
-            i++;
+            Token *t = add_token(v, TK_NUM, p);
+            t->val = strtol(p, &p, 10);
             continue;
         }
 
         error_at(p, "Couldn't tokenize");
     }
 
-    tokens[i].ty = TK_EOF;
-    tokens[i].input = p;
+    add_token(v, TK_EOF, p);
+    return v;
 }
+
 
 Node *new_node(int ty, Node *lhs, Node *rhs) {
     Node *node = malloc(sizeof(Node));
@@ -228,7 +214,7 @@ Node *new_node_num(int val) {
 }
 
 int consume(int ty) {
-    if (tokens[pos].ty != ty)
+    if (((Token *)tokens->data[pos])->ty != ty)
         return 0;
     pos++;
     return 1;
@@ -306,15 +292,15 @@ Node *term() {
     if (consume('(')) {
         Node *node = expr();
         if (!consume(')'))
-            error_at(tokens[pos].input,
+            error_at(((Token *)tokens->data[pos])->input,
                     "Open paren has no corresponding closing paren.");
         return node;
     }
 
-    if (tokens[pos].ty == TK_NUM)
-        return new_node_num(tokens[pos++].val);
+    if (((Token *)tokens->data[pos])->ty == TK_NUM)
+        return new_node_num(((Token *)tokens->data[pos++])->val);
 
-    error_at(tokens[pos].input,
+    error_at(((Token *)tokens->data[pos])->input,
             "Neither a number nor an open paren.");
 }
 
@@ -388,7 +374,6 @@ Vector *new_vector() {
 void vec_push(Vector *vec, void *elem) {
     if (vec->capacity == vec->len) {
         vec->capacity *= 2;
-        // XXX: 異なるサイズの整数からポインタへのキャスト
         vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
     }
     vec->data[vec->len++] = elem;
@@ -407,6 +392,7 @@ void runtest() {
     expect(__LINE__, 0, vec->len);
 
     for (int i = 0; i < 100; i++)
+        // todo: Cast from different size int to a pointer.
         vec_push(vec, (void *)i);
 
     expect(__LINE__, 100, vec->len);
@@ -415,4 +401,12 @@ void runtest() {
     expect(__LINE__, 99, (long)vec->data[99]);
 
     printf("OK\n");
+}
+
+Token *add_token(Vector *v, int ty, char *input) {
+    Token *t = malloc(sizeof(Token));
+    t->ty = ty;
+    t->input = input;
+    vec_push(v, t);
+    return t;
 }
